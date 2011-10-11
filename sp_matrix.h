0a8bec1c61f6eff30f643130331346b7d925050c
Alexey Veretennikov <txm.fourier@gmail.com>
2011-10-11 22:12:15 +0200

    Removed old declaration

--- sp_matrix.h
+++ sp_matrix.h
@@ -252,9 +252,6 @@ void sp_matrix_yale_mv(sp_matrix_yale_ptr self,double* x, double* y);
  * 0 in case of error
  */
 int* sp_matrix_yale_etree(sp_matrix_yale_ptr self);
-int* cs_etree(sp_matrix_yale_ptr self);
-
-
 
 /*
  * Solves SLAE L*x = b
